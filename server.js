2df0b9324cae464403cacb9054c088629b9e7fbf
ManishS6 <63586933+ManishS6@users.noreply.github.com>
2021-02-19 14:37:48 +0530

    Compeleted URL Shortener

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongodb = require('mongodb');
 const dns = require('dns');
 const bodyParser = require('body-parser');
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 3000;
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI,{
+const { RSA_NO_PADDING } = require('constants');
+mongoose.connect(uri,{
   useUnifiedTopology:true,
   useNewUrlParser: true
 });
+
 let Url;
 
 const Schema = mongoose.Schema;
@@ -21,9 +26,10 @@ const newUrlSchema = new Schema({
 Url = mongoose.model('Url',newUrlSchema);
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -37,34 +43,27 @@ app.get('/api/hello', function(req, res) {
 });
 });
 
-app.get("/api/shorturl/:id",(req,res)=>{
+app.get("/api/shorturl/:id",async (req,res)=>{
     let id = req.params.id;
-    // console.log(id);
-    Url.findOne({
-      short_url: id
-    },(err,data)=>{
-      if(err) return console.error(err);
-      res.redirect(data.original_url);
-    });
+    try {
+      const urlParams = await Url.findOne({
+        short_url: id
+      })
+      if (urlParams){
+        return res.redirect(data.original_url);
+      } else {
+        return res.status(404).json('No URL found');
+      }
+      
+    } catch (err) {
+      console.log(err);
+      res.status(500).json('Server error');
+    }
 });
-
-app.get("/api/test/:id",(req,res)=>{
-    let id = req.params.id;
-    // console.log(id);
-    Url.findOne({
-      short_url: id
-    },(err,data)=>{
-      if(err) return console.error(err);
-      console.log(data);
-      // res.redirect(data.original_url);
-    });
-});
-
-app.use(bodyParser.urlencoded({extended:false}));
 
 // The Following Code works fine !!!
 
-app.post("/api/shorturl/new",(req,res)=>{
+app.post("/api/shorturl/new",async (req,res)=>{
   let url_posted = req.body.url;
   // testing if the url had http:// or https://
   if(/^https?:\/\//i.test(url_posted)==false){
@@ -74,49 +73,41 @@ app.post("/api/shorturl/new",(req,res)=>{
   }
   let res1 = url_posted.replace(/^https?:\/\//i,'');
   // using the dns lookup function to check if the url requested for is valid or not
-  dns.lookup(res1,(err)=>{
-    if (err == null) {
-      var ID = function () {
-        return Math.random().toString(36).substr(2, 9);
-      };
-      var identifier = ID(1);
-      // console.log(identifier);
-      
-      Url.findOne({
-        original_url: url_posted
-      },(err,data)=>{
-        if(err) return console.error(err);
-        if(data!=null){
-          console.log(data);
-          return res.json({
-            original_url: data.original_url,
-            short_url: data.short_url
-          })
-        } else {
-          var test = new Url({
-            original_url: url_posted,
-            short_url: identifier
+  if(res1){
+    dns.lookup(res1,async (err,address,family)=>{
+      if(err){
+        res.json({error: 'invalid URL'});
+      } else {
+        try {
+          let findOne = await Url.findOne({
+            original_url: url_posted
           });
-          test.save()
-              .then(item => {
-              res.send("item saved to database");
-              })
-              .catch(err => {
-              res.status(400).send("unable to save to database");
-              });
-          res.json({
-            original_url: url_posted,
-            short_url: identifier
-          });
+          if(findOne){
+            res.json({
+              original_url: findOne.original_url,
+              short_url: findOne.short_url
+            })
+          } else {
+            var identifier = ID(1);
+            findOne = new Url({
+              original_url: url_posted,
+              short_url: identifier
+            });
+            await findOne.save();
+            res.json({
+              original_url: findOne.original_url,
+              short_url: findOne.short_url
+            })
+          }
+        } catch (err) {
+          console.error(err);
+          res.status(500).json('Server error...');
         }
-      });
-    }
-    else {
-      res.json({
-        "error": 'Invalid URL'
-      });
-    }
-  });
+      }
+    });
+  } else {
+    res.json({error: 'invalid URL'});
+  }
 });
 
 app.listen(port, function() {
